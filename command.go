bb7baf7575885f08239392f277e90c2765b88a72
Bryan Matsuo <bryan.matsuo@gmail.com>
2013-01-02 01:13:40 -0800

    re-write interface and add a help command

--- command.go
+++ command.go
@@ -9,21 +9,64 @@ import (
 
 var CommandHost string
 
+type UsageOptions []struct{ option, desc string }
+
 type Command struct {
-	name   string
-	action func(args []string)
+	name    string
+	desc    string
+	usage   string
+	options UsageOptions
+	action  func(args []string) error
 }
 
-func iTunesScript(script string) string {
-	if CommandHost != "" {
-		return fmt.Sprintf(`tell application "iTunes" of machine %q to %s`, CommandHost, script)
+type Script []string
+
+func (s Script) lines() []string {
+	if len(s) == 0 {
+		panic("empty script")
 	}
-	return fmt.Sprintf(`tell application "iTunes" to %s`, script)
+
+	lines := make([]string, 0, 2)
+	tell := `tell application "iTunes"`
+	if CommandHost != "" {
+		tell += fmt.Sprintf(` of machine %q`, CommandHost)
+	}
+	if len(s) == 1 {
+		tell += " to " + s[0]
+		lines = append(lines, tell)
+	} else {
+		lines = append(lines, tell)
+		lines = append(lines, s...)
+		lines = append(lines, "end tell")
+	}
+	return lines
 }
 
-func iTunesCommand(w io.Writer, script string, args ...string) *exec.Cmd {
-	command := exec.Command("osascript", "-e", iTunesScript(script))
+func (s Script) args() []string {
+	var args []string
+	for _, line := range s.lines() {
+		args = append(args, "-e", line)
+	}
+	return args
+}
+
+func (s Script) Command(w io.Writer, args ...string) *exec.Cmd {
+	command := exec.Command("osascript", append(s.args(), args...)...)
 	command.Stdout = w
 	command.Stderr = os.Stderr
 	return command
+}
+
+func (s Script) Run(args ...string) error {
+	return s.Command(os.Stdout, args...).Run()
+}
+
+func (s Script) Output(args ...string) ([]byte, error) {
+	return s.Command(nil, args...).Output()
+}
+
+func (s Script) OutputString(args ...string) (string, error) {
+	p, err := s.Output(args...)
+	str := string(p)
+	return str, err
 }
