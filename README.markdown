6d3de233e4f0e10963c9d6b59ad9035684fac3c0
Bryan Matsuo <bryan.matsuo@gmail.com>
2013-01-02 21:37:30 -0800

    documentation

---
+++ README.markdown
@@ -0,0 +1,6 @@
+##Control iTunes from the terminal
+
+    tuner play
+    tuner rate 5
+
+##[Documentation](godoc.org/github.com/bmatsuo/go-itunes)
---
+++ doc.go
@@ -0,0 +1,16 @@
+/*
+Tuner controls iTunes via Applescript from the command line.
+
+	tuner play
+	tuner next
+	tuner rate 5
+
+It also controls iTunes on remote machines (this method seems pretty unstable).
+
+	tuner -host eppc://10.0.0.34 pause
+
+For help regarding a specific command 
+
+	tuner help [COMMAND]
+*/
+package documentation
