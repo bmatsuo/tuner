c2c4ca54a1b4f03083b229bcc6aa7ed7d750370a
Bryan Matsuo <bryan.matsuo@gmail.com>
2013-01-02 22:20:18 -0800

    better formatted godoc

--- doc.go
+++ doc.go
@@ -1,15 +1,40 @@
 /*
 Tuner controls iTunes via Applescript from the command line.
 
+Examples
+
+Control local iTunes
+
 	tuner play
 	tuner next
 	tuner rate 5
 
-It also controls iTunes on remote machines (this method seems pretty unstable).
+Or control iTunes on remote machines (this is flakey)
 
 	tuner -host eppc://10.0.0.34 pause
 
-For help
+Commands
+
+The following commands are available in tuner
+
+	help    show this list of commands
+	info    current track info
+	mute    mute/unmute playback
+	next    skip to the next track
+	open    open iTunes
+	pause   pause the current track
+	play    start playing the current track
+	prev    skip to the previous track
+	quit    quit iTunes
+	rate    adjust rating of the current track
+	status  player status (playing, paused, stopped)
+	stop    stop playback
+	vol     adjust playback volume
+
+
+Help
+
+Command usage is available through the "help" command
 
 	tuner help [COMMAND]
 */
